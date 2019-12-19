a89a079de743e05d6c2a7d40449202cb6803eb31
mazyn <gilmarfilho75@gmail.com>
2019-12-19 17:39:21 -0300

    initial commit

---
+++ App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import InstaClone from './src/InstaClone';
+
+export default function App() {
+  return <InstaClone />;
+}
